66c5dd29b34e4597f076def2f3656443f5b962d1
jihadul4825 <jihadul4825@gmail.com>
2025-09-27 23:23:37 +0600

    update9

--- docDetails.js
+++ docDetails.js
@@ -102,7 +102,9 @@ const handleAppointment = () => {
         body: JSON.stringify(info),
     })
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            window.location.href = `pdf.htmldoctorId=${param}`
+        });
 };
 
 
@@ -115,57 +117,7 @@ const loadPatientId = () => {
     });
 }
 
-
-const handlePdf = () => {
-    const doctor_id = new URLSearchParams(window.location.search).get("doctorId");
-    const user_id = localStorage.getItem("user_id");
-    fetch(`https://jsonplaceholder.typicode.com/doctor/list/${param}`)
-    .then((res) => res.json())
-    .then((data) => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${user_id})`)
-            .then(res => res.json())
-            .then((pdData) => {
-            const newData = [data, pdData];
-            const parent = document.getElementById("pdf-container");
-            const div = document.createElement("div");
-            div.innerHTML = `
-                <div class="pd d-flex justify-content-around align-items-center p-5">
-                    <div class="patient doctor p-5">
-                        <h2>${newData[1].username}</h2>
-                        <h3>${newData[1].first_name} ${newData[1].last_name}</h3>
-                        <h3>${newData[1].email}</h3>
-                    </div>
-                    <div class="doctor p-5">
-                        <img class="w-25 border-rounded-2" src=${newData[0].image} alt=""/>
-                        <h2 class="doc-name">Name: ${newData[0].full_name}</h2>
-                        <h3>designation: ${
-                            newData[0].designation.map((item) => {
-                                return `${item}`;
-                            })
-                        }</h3>
-                        <h5>specialization: ${
-                            newData[0].specialization.map((item) => {
-                                return `${item}`;
-                            })
-                        }</h5>
-                    </div>
-                </div>
-                <input id="pdf-symptom" class="symtom" type="text">
-                <h1 id="pdf-fees" class="text-center p-2 mt-3">Fees: ${newData[0].fee} BDT</h1>
-            `
-
-            parent.appendChild(div);
-        });
-    });
-
-    
-    
-
-};
-
-
-
 loadPatientId();
 getparams();
 loadTime();
-handlePdf();
+
